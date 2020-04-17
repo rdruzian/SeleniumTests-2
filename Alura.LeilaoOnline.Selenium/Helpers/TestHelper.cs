79d2b1b602eb71478281b86fcad3971446ca4ed4
rdruzian <rendruzian@gmail.com>
2020-04-16 23:28:24 -0300

    Alterado caminho para o driver do Chrome

--- Alura.LeilaoOnline.Selenium/Helpers/TestHelper.cs
+++ Alura.LeilaoOnline.Selenium/Helpers/TestHelper.cs
@@ -8,6 +8,7 @@ namespace Alura.LeilaoOnline.Selenium.Helpers
 {
     public static class TestHelper
     {
-        public static string PastaDoExecutavel => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //public static string PastaDoExecutavel => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string PastaDoExecutavel => Path.GetDirectoryName("C:\\Users\\renat\\Downloads\\chromedriver.exe"); 
     }
 }
