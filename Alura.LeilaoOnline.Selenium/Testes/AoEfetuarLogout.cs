958da57703defe667916ad1ca81c3a71d65dfbd0
rdruzian <rendruzian@gmail.com>
2020-04-16 23:01:44 -0300

    Aula 5

---
+++ Alura.LeilaoOnline.Selenium/Testes/AoEfetuarLogout.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarLogout(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        public void LoginValidoHome()
+        {
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
