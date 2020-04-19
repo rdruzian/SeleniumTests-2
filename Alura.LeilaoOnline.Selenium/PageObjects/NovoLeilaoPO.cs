8f5013150bfb914e1cec1378d482d313c6078358
rdruzian <rendruzian@gmail.com>
2020-04-18 22:14:22 -0300

    Cria;áo do PageObject para adicionar um novo leiláo

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class NovoLeilaoPO
+    {
+        private IWebDriver driver;
+        private By byInputTitulo;
+        private By byInputDescricao;
+        private By byInputCategoria;
+        private By byInputValorInicial;
+        private By byInputImagem;
+        private By byInputInicioPregao;
+        private By byInputTerminoPregao;
+
+        public NovoLeilaoPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byInputTitulo = By.Id("Titulo");
+            byInputDescricao = By.Id("Descriao");
+            byInputCategoria = By.Id("Categoria");
+            byInputValorInicial = By.Id("ValorInicial");
+            byInputImagem = By.Id("Imagem");
+            byInputInicioPregao = By.Id("InicioPregao");
+            byInputTerminoPregao = By.Id("TerminoPregao");
+        }
+
+        public void Visitar()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/Leiloes/Novo");
+        }
+
+        public void PreencheForm(string titulo, string descricao, string categoria, double valor, string imagem, DateTime inicio, DateTime termino)
+        {
+            driver.FindElement(byInputTitulo).SendKeys(titulo);
+            driver.FindElement(byInputDescricao).SendKeys(descricao);
+            driver.FindElement(byInputCategoria).SendKeys(categoria);
+            driver.FindElement(byInputValorInicial).SendKeys(valor.ToString());
+            driver.FindElement(byInputImagem).SendKeys(imagem);
+            driver.FindElement(byInputInicioPregao).SendKeys(inicio.ToString());
+            driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString());
+        }
+    }
+}
