ea4903959854da073ee17a21533aa1cffd5c4c9c
rdruzian <rendruzian@gmail.com>
2020-04-21 19:55:20 -0300

    Cirado testes para verificar responsibilidade do site

---
+++ Alura.LeilaoOnline.Selenium/PageObjects/MenuNaoLogadoPO.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class MenuNaoLogadoPO
+    {
+        private IWebDriver driver;
+        private By byMenuMobile;
+
+        public bool MenuMobileVisivel {
+            get
+            {
+                var elemento = driver.FindElement(byMenuMobile);
+                return elemento.Displayed;
+            }
+        }
+
+        public MenuNaoLogadoPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byMenuMobile = By.ClassName("sidenav-trigger");
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/Testes/AoNavegarParaHomeMobile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Alura.LeilaoOnline.Selenium.Helpers;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    public class AoNavegarParaHomeMobile : IDisposable
+    {
+        private ChromeDriver driver;
+
+        public AoNavegarParaHomeMobile()
+        {
+            
+        }
+
+        [Fact]
+        public void DadaLargura992MenuMobile()
+        {
+            var deviceSettings = new ChromeMobileEmulationDeviceSettings();
+            deviceSettings.Width = 992;
+            deviceSettings.Height = 800;
+            deviceSettings.UserAgent = "Customizada";
+
+            var options = new ChromeOptions();
+            options.EnableMobileEmulation(deviceSettings);
+
+            driver = new ChromeDriver(TestHelper.PastaDoExecutavel, options);
+
+            var homePO = new HomeNaoLogadaPO(driver);
+            homePO.Visitar();
+
+            Assert.True(homePO.Menu.MenuMobileVisivel);
+        }
+
+        [Fact]
+        public void DadaLargura993NaoDeveMostrarMenuMobile()
+        {
+            var deviceSettings = new ChromeMobileEmulationDeviceSettings();
+            deviceSettings.Width = 993;
+            deviceSettings.Height = 800;
+            deviceSettings.UserAgent = "Customizada";
+
+            var options = new ChromeOptions();
+            options.EnableMobileEmulation(deviceSettings);
+
+            driver = new ChromeDriver(TestHelper.PastaDoExecutavel, options);
+
+            var homePO = new HomeNaoLogadaPO(driver);
+            homePO.Visitar();
+
+            Assert.False(homePO.Menu.MenuMobileVisivel);
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
